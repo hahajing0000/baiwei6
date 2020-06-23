4a80ed480632917a20fa90ca2c7448fbe5cb5adf
hahajing <444511958@qq.com>
2020-06-23 17:50:19 +0800

    加入titlebar未完成

---
+++ wiget/src/main/java/com/zy/wiget/TitleBar.java
@@ -0,0 +1,29 @@
+package com.zy.wiget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author:zhangyue
+ * @date:2020/6/23
+ * 标题栏
+ */
+public class TitleBar extends LinearLayout {
+    public TitleBar(Context context) {
+        super(context);
+    }
+
+    public TitleBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
